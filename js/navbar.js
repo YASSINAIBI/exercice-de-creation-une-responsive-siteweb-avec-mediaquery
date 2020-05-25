1084c5b3a24c6a9ff775d52040990c453de7cc0e
yassineaibi <aibiyassin8@gmail.com>
2020-05-25 18:53:58 +0000

    @media

---
+++ js/navbar.js
@@ -0,0 +1,7 @@
+let navicon = document.getElementById('Mynavicon');
+
+let navlist = document.getElementById('Mynavlist');
+
+navicon.addEventListener('click', function(){
+    navlist.classList.toggle('nav');
+});
